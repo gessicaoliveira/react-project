6096ce872dd2a4ca6759d1e7b2e7f7c4a942af50
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2023-09-21 01:57:43 +0000

    atualização no card

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
     <>
       <div className="card">
         <h2>Yugi Itadori</h2>
+        <p>Hospedeiro do Sukuna</p>
+        <p>Habilidades: Força sobre-humana</p>
         <img src="https://i.pinimg.com/originals/85/b3/05/85b305c18de2de0d67b56a87918bbf68.jpg" />
       </div>
     </>
