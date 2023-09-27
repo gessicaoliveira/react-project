e17050f90451ebafe3e6b8d3f02147e51131b1e3
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2023-09-27 23:58:59 +0000

    update card and new component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,31 @@
+import Tag from "../Tag/Tag";
 import "./Card.css";
 
 export default function Card(props) {
   const item = props.item;
+
+  // Garantir que temos sempre um array em tags
+
+  // Jeito literal
+  // let tags = item.tags;
+
+  // if (!tags) {
+  //   tags = [];
+  // }
+
+  // Jeito otimizado
+  const tags = item.tags || [];
 
   return (
     <div className="card">
       <h2>{item.name}</h2>
 
       <div className="tags">
-        <span className="tag">Anime: {item.anime}</span>
-        <span className="tag">{item.role}</span>
-        <span className="tag">Habilidades: {item.habilities}</span>
+        {tags.map(function (tag, index) {
+          return <Tag text={tag} key={`card_tag_${index}`} />;
+        })}
       </div>
+
       <img src={item.imageUrl} />
     </div>
   );
