a2a1a0addd3be30bbb4027a08057c06c024d9808
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2023-09-28 01:33:28 +0000

    update tags and card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import Tag from "../Tag/Tag";
+import Tag from "./Tag/Tag";
 import "./Card.css";
 
 export default function Card(props) {
@@ -12,9 +12,18 @@ export default function Card(props) {
   // if (!tags) {
   //   tags = [];
   // }
-
   // Jeito otimizado
-  const tags = item.tags || [];
+  //const tags = item.tags || [];
+
+  const tags = [
+    `Status: ${item.status}`,
+    `Species: ${item.species}`,
+    `Species: ${item.origin.name}`,
+  ];
+  // console.log(tags);
+
+  // TODO: Fazer as tags funcionarem na nova estrutura de dados recebida da API
+  //        Exibir Tag de Status, Species e Origin
 
   return (
     <div className="card">
@@ -26,7 +35,7 @@ export default function Card(props) {
         })}
       </div>
 
-      <img src={item.imageUrl} />
+      <img src={item.image} />
     </div>
   );
 }
