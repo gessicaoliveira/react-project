2579ffe9fa230e93daa71ffc2d13194180c9cc25
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2023-10-02 23:59:33 +0000

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
 import "./Header.css";
 
 export default function Header() {
-  return <header className="header">Header está funcionando!!</header>;
+  return (
+    <header className="header">
+      <a href="/" className="link-logo">
+        <img
+          src="https://oceanbrasil.com/assets/logo.svg"
+          alt="Logo da Ocean"
+        />
+      </a>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/character-create">Criar Personagem</a>
+      </nav>
+    </header>
+  );
 }
